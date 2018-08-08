43f5f5b8a39a56731b4398fc66769f7eea235682
Thomas Keller <thomas.e.keller@gmail.com>
2018-08-08 16:32:50 -0400

    added slurm batch submit script and modified config.json, fingers crossed ready to submit ?

---
+++ snakemake_sub.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+# this file is snakemake_sub.sh
+#submit with
+# sbatch --cpus-per-task=2 --mem=8g snakemake.sh
+
+sbcmd="sbatch --cpus-per-task={threads} --mem={cluster.mem}"
+sbcmd+=" --time={cluster.time} --partition={cluster.partition}"
+sbcmd+=" --out={cluster.out} {cluster.extra}"
+snakemake -pr --keep-going --local-cores $SLURM_CPUS_PER_TASK \
+             --jobs 10 --cluster-config cluster.json --cluster "$sbcmd" \
+             --latency-wait 120 all
+
+snakemake -pr --keep-going --local-cores $SLURM_CPUS_PER_TASK \
+    --jobs 10 --cluster-config cluster.json --cluster "$sbcmd" \
+    --latency-wait 120 all
