35f4824d8882c2cfb05f8bd6def73d8e25a751e5
Thomas Keller <thomas.e.keller@gmail.com>
2018-08-13 17:17:49 -0400

    tried new submit script

--- snakemake_sub.sh
+++ snakemake_sub.sh
@@ -1,39 +1,25 @@
-#!/bin/bash
+#! /bin/bash
 
 #SBATCH --job-name=snakemake_gen
-#SBATCH --time=08:00:00
+#SBATCH --time=24:00:00
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=2
 #SBATCH --mem=8G
 #SBATCH -o output.%j.%N.txt
 #SBATCH -e error.%j.%N.txt
 
-#
-# check for clean
-#
-# https://snakemake.readthedocs.io/en/stable/project_info/faq.html#how-do-i-remove-all-files-created-by-snakemake-i-e-like-make-clean
-if [ "$1" == "clean" ]; then
-    echo 'rm $(snakemake --summary | tail -n+2 | cut -f1)'
-    snakemake --summary | tail -n+2 | cut -f1
-    rm -f $(snakemake --summary | tail -n+2 | cut -f1)
-    exit 0
-fi
+# this file is snakemake_sub.sh
+#submit with
+# sbatch --cpus-per-task=2 --mem=8g snakemake_sub.sh
+#taken from biowulf example
+#https://hpc.nih.gov/apps/snakemake.html
 
-#
-# launch snakemake to run jobs on UAB CHEAHA via SLURM
-#
-SM_PARAMS="job-name ntasks partition time mail-user mail-type error output"
-SM_ARGS="--cpus-per-task={threads} --mem={cluster.mem}"
-for P in ${SM_PARAMS}; do SM_ARGS="$SM_ARGS --$P={cluster.$P}"; done
-echo "SM_ARGS: ${SM_ARGS}"
-
-# our SLURM error/output paths expect a logs/ subdir in PWD
-mkdir -p logs
+sbcmd="sbatch --cpus-per-task={threads} --mem={cluster.mem}"
+sbcmd+=" --nodes={cluster.nodes} --ntasks={cluster.ntasks}"
+sbcmd+=" --time={cluster.time} --partition={cluster.partition}"
+sbcmd+=" --out={cluster.output}"
 
 snakemake --unlock
-snakemake \
-    $* \
-     --latency-wait 120 \
-    --jobs=10 \
-    --cluster-config cluster.json \
---cluster "sbatch $SM_ARGS"
+snakemake -pr --keep-going \
+             --jobs 10 --cluster-config cluster.json --cluster "$sbcmd" \
+             --latency-wait 120 all
---
+++ snakemake_sub_old.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#SBATCH --job-name=snakemake_gen
+#SBATCH --time=08:00:00
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=2
+#SBATCH --mem=8G
+#SBATCH -o output.%j.%N.txt
+#SBATCH -e error.%j.%N.txt
+
+#
+# check for clean
+#
+# https://snakemake.readthedocs.io/en/stable/project_info/faq.html#how-do-i-remove-all-files-created-by-snakemake-i-e-like-make-clean
+if [ "$1" == "clean" ]; then
+    echo 'rm $(snakemake --summary | tail -n+2 | cut -f1)'
+    snakemake --summary | tail -n+2 | cut -f1
+    rm -f $(snakemake --summary | tail -n+2 | cut -f1)
+    exit 0
+fi
+
+#
+# launch snakemake to run jobs on UAB CHEAHA via SLURM
+#
+SM_PARAMS="job-name ntasks partition time mail-user mail-type error output"
+SM_ARGS="--cpus-per-task={threads} --mem={cluster.mem}"
+for P in ${SM_PARAMS}; do SM_ARGS="$SM_ARGS --$P={cluster.$P}"; done
+echo "SM_ARGS: ${SM_ARGS}"
+
+# our SLURM error/output paths expect a logs/ subdir in PWD
+mkdir -p logs
+
+snakemake --unlock
+snakemake \
+    $* \
+     --latency-wait 120 \
+    --jobs=10 \
+    --cluster-config cluster.json \
+--cluster "sbatch $SM_ARGS"
